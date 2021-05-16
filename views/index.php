96678f656ecccd6e50f09a48ce6e79e398cdbdc8
Andrea Pozza <andrea.pozza@hotmail.com>
2021-05-16 12:43:31 +0200

    inizio index view

--- views/index.php
+++ views/index.php
@@ -1,1 +1,68 @@
-<h1>index</h1>
+<!DOCTYPE html>
+<html lang="it">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rapporto di manutenzione - UNION</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+        integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w=="
+        crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
+        integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <!-- <script>
+        setInterval(() => {
+            fetch(window.location)
+                .then(r => r.text())
+                .then(data => {
+                    const html = new DOMParser().parseFromString(data, "text/html");
+                    const bodyOld = document.querySelector('body').innerHTML
+                    const bodyNew = html.querySelector('body').innerHTML
+                    if (bodyNew != bodyOld) window.location.reload(true);
+                })
+        }, 1000)
+
+    </script> -->
+
+    <style>
+        .bg-light-1 {
+            background-color: #ffffffd1!important;
+        }
+        body > div.position-fixed:before {
+            content: '';
+            width: 100%; height: 100%;
+            background-color: black;
+            opacity: .7;
+            position: absolute;
+            top: 0; left: 0;
+            z-index: -1;
+        }
+    </style>
+
+</head>
+
+<body>
+    <div class="w-100 vh-100 position-fixed" style="background: url(https://divisare-res.cloudinary.com/images/c_limit,f_auto,h_2000,q_auto,w_3000/v1/project_images/5158077/0904_Edificio_industriale_UNION_Masserano_Biella_23/daniela-saviola-michele-reginaldi-corrado-annoni-quattroassociati-union-industries.jpg) center/cover;">
+        <div class="container mt-5 p-5 bg-light-1 rounded-lg">
+            <div class="row">
+                <div class="col-12 col-sm-6">
+                    Prova
+                </div>
+                <div class="col-12 col-sm-6">
+                    Prova
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
