515f735c25300eedc11d22ada1d3ca1f8c531459
Andrea Pozza <andrea.pozza@hotmail.com>
2021-05-22 10:27:03 +0200

    fix: change pull.php

--- pull.php
+++ pull.php
@@ -1,2 +1,22 @@
-<?php shell_exec( 'git pull --rebase' ); ?>
-<h2>pulled</h2>
+<?php 
+echo '<hr>';
+echo '<p>$ git fetch origin; git reset --hard origin/main<p>';
+echo '<hr>';
+exec( 'git fetch origin; git reset --hard origin/main', $output1 ); 
+foreach ($output1 as $string) {
+    echo "<p>$string</p>";
+}
+echo '<hr>';
+echo '<p>$ git status</p>';
+echo '<hr>';
+exec( 'git status', $output2 ); 
+foreach ($output2 as $string) {
+    echo "<p>$string</p>";
+}
+echo '<hr>';
+echo '<p>$ git log -n 3</p>';
+echo '<hr>';
+exec( 'git log -n 3', $output3 ); 
+foreach ($output3 as $string) {
+    echo "<p>$string</p>";
+}
